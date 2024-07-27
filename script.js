4ccc6e2980c36fd6d1959aea6d7de6f4691b8985
RipeRagusta <132228468+RipeRagusta@users.noreply.github.com>
2024-07-27 17:40:13 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,139 +1,26 @@
-window.addEventListener("mousemove", logoOne);
-window.addEventListener("click", removePhoto);
-
-function expandPhoto(number)
-{
-	displayedTimer = true; 
-	document.getElementById("expanded").style.display = "block";
-	setTimeout(() => displayedTiming(), 50);
-}
-
-function displayedTiming()
-{
-	displayedTimer = false;
-}
-
-function removePhoto()
-{	
-	if(displayedTimer == false)
-	{
-		document.getElementById("expanded").style.display = "none";
-	}
-}
-
-function logoOne(){
-	if (window.innerWidth >= 968)
-	{
-    	var x = window.innerWidth / 2;
-		var mouseX = event.clientX;
-
-		var logo = document.querySelector("#expanded");
-		var referenceY = logo.getBoundingClientRect();
-
-		var y = referenceY.top + (referenceY.height / 2);
-		var mouseY = event.clientY;
-		var atanPositionX;
-		var atanPositionY;
-
-		if(mouseX <= x)
-		{
-			atanPositionX = x - mouseX;
-		}
-
-		if(mouseX > x)
-		{
-			atanPositionX = mouseX - x;
-		}
-
-		if(mouseY <= y)
-		{
-			atanPositionY = y - mouseY;
-		}
-
-		if(mouseY > y)
-		{
-			atanPositionY = mouseY - y;
-		}
-
-		var angle = Math.atan2(atanPositionY, atanPositionX) * 180 / Math.PI;
-		var realAngle;
-
-		if(mouseX > x && mouseY <= y)
-		{
-			realAngle = 90 - angle;
-		}
-		if(mouseX > x && mouseY > y)
-		{
-			realAngle = 90 + angle;
-		}
-		if(mouseX <= x && mouseY > y)
-		{
-			realAngle = 180 + (90 - angle);
-		}
-		if(mouseX <= x && mouseY <= y)
-		{
-			realAngle = 270 + angle;
-		}
-
-		if(realAngle > 337.5 && realAngle <=360 || realAngle <=22.5)
-		{
-			document.getElementById("expanded").style.transform = "translate(-50%, -50%) perspective(3000px) rotateX(15deg)";
-		}
-		if(realAngle > 22.5 && realAngle <= 67.5)
-		{
-			document.getElementById("expanded").style.transform = "translate(-50%, -50%) perspective(3000px) rotateY(10deg) rotateX(10deg)";
-		}
-		if(realAngle > 67.5 && realAngle <= 112.5)
-		{
-			document.getElementById("expanded").style.transform = "translate(-50%, -50%) perspective(3000px) rotateY(15deg)";
-		}
-		if(realAngle > 112.5 && realAngle <= 157.5)
-		{
-			document.getElementById("expanded").style.transform = "translate(-50%, -50%) perspective(3000px) rotateY(10deg) rotateX(-10deg)";
-		}
-		if(realAngle > 157.5 && realAngle <= 202.5)
-		{
-			document.getElementById("expanded").style.transform = "translate(-50%, -50%) perspective(3000px) rotateX(-15deg)";
-		}
-		if(realAngle > 202.5 && realAngle <= 247.5)
-		{
-			document.getElementById("expanded").style.transform = "translate(-50%, -50%) perspective(3000px) rotateY(-10deg) rotateX(-10deg)";
-		}
-		if(realAngle > 247.5 && realAngle <= 292.5)
-		{
-			document.getElementById("expanded").style.transform = "translate(-50%, -50%) perspective(3000px) rotateY(-15deg)";
-		}
-		if(realAngle > 292.5 && realAngle <= 337.5)
-		{
-			document.getElementById("expanded").style.transform = "translate(-50%, -50%) perspective(3000px) rotateY(-10deg) rotateX(10deg)";
-		}
-    }
-	else
-	{
-		document.getElementById("expanded").style.transform = "translate(-50%, -50%) perspective(3000px) rotateY(0deg) rotateX(0deg)";
-	}
-
-}
-
-var consitstent = false;
-
 function preview(number)
 {
+	var consitstent = false;
 	consitstent = true;
-	setTimeout(() => showGif(number), 667);
-}
 
-function showGif(passedNumb)
-{
-	if(consitstent == true)
+	element = document.getElementById("image-" + number);
+
+	element.addEventListener('mouseout', () => 
 	{
-		document.getElementById("image-" + passedNumb).src = "gif-" + passedNumb + ".GIF";
-	}
-	
-}
+  		consitstent = false;
+	});
 
-function reset(number)
-{
-	consitstent = false;
-	document.getElementById("image-" + number).src = "image-" + number + ".jpg";
+	setTimeout(() => 
+	{
+		if(consitstent == true)
+		{
+			document.getElementById("image-" + number).src = "gif-" + number + ".GIF";
+		}
+	}, 667);
+
+	element.addEventListener('mouseout', () => 
+	{
+		document.getElementById("image-" + number).src = "image-" + number + ".jpg";
+	});
+}" + number).src = "image-" + number + ".jpg";
 }
